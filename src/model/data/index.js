431a6a6d0bd8af3de425c95a98a3b2fad3b5f6c9
FurqanKhurrum <furqan.khurrum99@gmail.com>
2025-08-04 10:48:49 -0400

    Add HTPASSWD_FILE for basic auth in production

--- src/model/data/index.js
+++ src/model/data/index.js
@@ -1,13 +1,5 @@
 // src/model/data/index.js
 
-const logger = require('../../logger');
-
-// Use AWS backend if S3 bucket name is configured
-// (AWS credentials not required when using IAM roles in ECS)
-if (process.env.AWS_S3_BUCKET_NAME) {
-  logger.info(`Using AWS backend with S3 bucket: ${process.env.AWS_S3_BUCKET_NAME}`);
-  module.exports = require('./aws');
-} else {
-  logger.info('Using memory backend');
-  module.exports = require('./memory');
-}
+// If the environment sets an AWS Region, we'll use AWS backend
+// services (S3, DynamoDB); otherwise, we'll use an in-memory db.
+module.exports = process.env.AWS_REGION ? require('./aws') : require('./memory');
